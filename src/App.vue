6032f3893ddc4d5b17fb9d96ae3f3055dee45d07
Fabio Resende <fabio.resende@gmail.com>
2022-11-08 09:38:07 -0300

    testes vite3

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+import Youtube from "./components/Youtube.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div>
+    <header>
+      <img
+        alt="Vue logo"
+        class="logo"
+        src="./assets/logo.svg"
+        width="125"
+        height="125"
+      />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+      <div class="wrapper">
+        <HelloWorld msg="You did it!" />
+      </div>
+    </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+      <TheWelcome />
+      <Youtube />
+    </main>
+  </div>
 </template>
 
 <style scoped>
---
+++ src/components/Youtube.vue
@@ -0,0 +1,15 @@
+<template>
+  <lite-youtube
+    videoid="VLrYOji75Vc"
+    playlistid="PL-G5r6j4GptH5JTveoLTVqpp7w2oc27Q9"
+  ></lite-youtube>
+</template>
+
+<script>
+import "@justinribeiro/lite-youtube";
+export default {
+  name: "Youtube",
+};
+</script>
+
+<style></style>
