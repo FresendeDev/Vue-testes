7b5dcfcbb4b989ccc1a879240eb6186bb291248a
Fabio Resende <fabio.resende@gmail.com>
2022-11-08 12:05:09 -0300

    inicio testes

--- src/components/Youtube.vue
+++ src/components/Youtube.vue
@@ -1,8 +1,18 @@
 <template>
-  <lite-youtube
-    videoid="VLrYOji75Vc"
-    playlistid="PL-G5r6j4GptH5JTveoLTVqpp7w2oc27Q9"
-  ></lite-youtube>
+  <div>
+    <lite-youtube
+      videoid="VLrYOji75Vc"
+      playlistid="PL-G5r6j4GptH5JTveoLTVqpp7w2oc27Q9"
+    ></lite-youtube>
+
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-12 col-md-4 text-bg-primary">Column</div>
+        <div class="col-12 col-md-4 text-bg-primary">Column</div>
+        <div class="col-12 col-md-4 text-bg-primary">Column</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
