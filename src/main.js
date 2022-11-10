1693f5702dc0e1081e414902e3d031e60f0c8aaf
Fabio Resende <fabio.resende@gmail.com>
2022-11-10 19:33:29 -0300

    testes

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import App from "./App.vue";
 // import "./assets/main.scss";
 import "./assets/main.scss";
 // Import all of Bootstrap's JS
-// import * as bootstrap from "bootstrap";
+import * as bootstrap from "bootstrap";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 
